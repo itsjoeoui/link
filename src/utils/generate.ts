c78786c91d1a1e65432e89e904831a409248384e
Joey Yu <joey@jyu.dev>
2023-11-12 19:59:57 -0500

    8 chars is good enough

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -2,10 +2,7 @@ export function generateRandomAlias() {
   let randomString = "";
   const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (let i = 0; i < 16; i++) {
-    if (i > 0 && i % 4 === 0) {
-      randomString += "-";
-    }
+  for (let i = 0; i < 8; i++) {
     randomString += characters.charAt(
       Math.floor(Math.random() * characters.length),
     );
